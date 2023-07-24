3a45f95a747cd168dec3017062dabc065dcad336
Idrissa <idrisabkar2018@gmail.com>
2023-07-24 16:12:45 +0300

    guess my numbee

--- Guess-My-Number/script.js
+++ Guess-My-Number/script.js
@@ -1,13 +1,57 @@
 'use strict';
 
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+let hearts = 20;
+let high_s = 0;
 const user_g = document.getElementById('number');
 const btn = document.getElementById('button');
 const score = document.getElementsByClassName('score')[0];
-console.log(score);
+const message = document.getElementsByClassName('message')[0];
+let randomNumber = getRandomNumber(1, 20);
+let highScore = document.getElementsByClassName('highcore')[0];
+score.textContent = hearts;
 
-btn.addEventListener('click', () => {
-  score.textContent = user_g.value;
-});
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function reset() {
+  setTimeout(() => {
+    hearts = 20;
+    score.textContent = hearts;
+    document.body.style.backgroundColor = '#222';
+  }, 1000);
+}
+
+function check() {
+  if (hearts > high_s) {
+    high_s = hearts;
+    highScore.textContent = high_s;
+  }
+}
+
+function handleClick() {
+  if (hearts == 0) {
+    message.textContent = "🥵 You're a loser !!";
+    document.body.style.backgroundColor = 'red';
+    reset();
+  } else {
+    if (user_g.value == randomNumber) {
+      message.textContent = '✔️ Correct Number !';
+      document.body.style.backgroundColor = 'green';
+      reset();
+    } else if (user_g.value > randomNumber) {
+      hearts -= 1;
+      score.textContent = hearts;
+
+      message.textContent = ' ↗️ Too high !';
+    } else if (user_g.value < randomNumber) {
+      hearts -= 1;
+      score.textContent = hearts;
+
+      message.textContent = '↘️ Too low !';
+    }
+    check();
+  }
+}
+
+btn.addEventListener('click', handleClick);
